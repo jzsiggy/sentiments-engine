05651e1439eedaa214f8558bf9b89220c67813d9
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-22 16:09:57 -0200

    Flask CORS permission

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from analyze import compare, get_negative_headlines, get_positive_headlines
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return "Home"
 
-@app.route('/positive')
+@app.route('/reddit/positive')
 def positive():
     keyword = request.args['keyword']
     return jsonify(get_positive_headlines(keyword))
 
-@app.route('/negative')
+@app.route('/reddit/negative')
 def negative():
     keyword = request.args['keyword']
     return jsonify(get_negative_headlines(keyword))
