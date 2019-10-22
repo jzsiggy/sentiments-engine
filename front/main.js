44783c74663e885cb6b7a0fc9bf661003985189c
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-22 18:16:16 -0200

    Creating front-end request page

--- front/main.js
+++ front/main.js
@@ -1,9 +1,54 @@
-let request = new XMLHttpRequest
+removeAllChildren = (div) => {
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    };
+};
 
-request.open('GET', 'http://127.0.0.1:5000/reddit/positive?keyword=bitcoin', true);
-request.onload = function () {
-    let data = JSON.parse(this.response);
-    console.log(data);
-}
+getPositiveRequest = (keyword) => {
+    let positiveDiv = document.querySelector(".positive-headlines")
+    let request = new XMLHttpRequest
 
-request.send();
+    request.open('GET', `http://127.0.0.1:5000/reddit/positive?keyword=${keyword}`, true);
+    request.onload = function () {
+        let data = JSON.parse(this.response);
+        console.log(data);
+        for (index in data) {
+            let header = document.createElement("h4");
+            header.innerText = data[index]["headline"];
+            positiveDiv.appendChild(header);
+        };
+    };
+    
+    request.send();
+};
+
+getNegativeRequest = (keyword) => {
+    let negativeDiv = document.querySelector(".negative-headlines")
+    let request = new XMLHttpRequest
+
+    request.open('GET', `http://127.0.0.1:5000/reddit/negative?keyword=${keyword}`, true);
+    request.onload = function () {
+        let data = JSON.parse(this.response);
+        console.log(data);
+        for (index in data) {
+            let header = document.createElement("h4");
+            header.innerText = data[index]["headline"];
+            negativeDiv.appendChild(header);
+        };
+    };
+    
+    request.send();
+};
+
+const searchBtn = document.querySelector("#search-btn");
+
+searchBtn.addEventListener("click", () => {
+    document.querySelectorAll(".responses > div > div").forEach(div => {
+        removeAllChildren(div);
+    });
+    
+    const input = document.querySelector("#search");
+    console.log(input.value);
+    getPositiveRequest(input.value);
+    getNegativeRequest(input.value);
+});
