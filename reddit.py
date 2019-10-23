4e2d037214a9170540c165f095d3edf1f11ebcfc
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-23 19:37:23 -0200

    modifying reddit api call

--- reddit.py
+++ reddit.py
@@ -1,4 +1,4 @@
-from config import client_id, client_secret, user_agent
+from config import reddit_client_id, reddit_client_secret, reddit_user_agent
 import praw
 
 reddit = praw.Reddit(client_id=reddit_client_id,
@@ -7,7 +7,14 @@ reddit = praw.Reddit(client_id=reddit_client_id,
 
 
 def get_reddit_headlines(keyword):
-    headlines = set()
-    for submission in reddit.subreddit(keyword).hot(limit=200):
-        headlines.add(submission.title)
+    headlines = {}
+    index = 0
+    for submission in reddit.subreddit(keyword).hot(limit=10):
+        headlines["{}".format(index)] = {
+            "title" : submission.title,
+            "comments" : [comment.body for comment in submission.comments],
+            "url" : submission.url
+        }
+        index+=1
+
     return headlines
