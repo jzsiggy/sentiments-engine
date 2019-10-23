afcd077e112a67efb57555adc3690026cf675137
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-23 19:38:01 -0200

    sending polarity comparisson to front

--- front/main.js
+++ front/main.js
@@ -4,41 +4,32 @@ removeAllChildren = (div) => {
     };
 };
 
-getPositiveRequest = (keyword) => {
+parseRedditInfo = (keyword) => {
     let positiveDiv = document.querySelector(".positive-headlines")
-    let request = new XMLHttpRequest
-
-    request.open('GET', `http://127.0.0.1:5000/reddit/positive?keyword=${keyword}`, true);
-    request.onload = function () {
-        let data = JSON.parse(this.response);
-        console.log(data);
-        for (index in data) {
-            let header = document.createElement("h4");
-            header.innerText = data[index]["headline"];
-            positiveDiv.appendChild(header);
-        };
-    };
-    
-    request.send();
-};
-
-getNegativeRequest = (keyword) => {
     let negativeDiv = document.querySelector(".negative-headlines")
     let request = new XMLHttpRequest
 
-    request.open('GET', `http://127.0.0.1:5000/reddit/negative?keyword=${keyword}`, true);
+    request.open('GET', `http://127.0.0.1:5000/reddit?keyword=${keyword}`, true);
     request.onload = function () {
         let data = JSON.parse(this.response);
         console.log(data);
         for (index in data) {
+            let link = document.createElement("a");
+            link.setAttribute("href", data[index]["url"])
             let header = document.createElement("h4");
-            header.innerText = data[index]["headline"];
-            negativeDiv.appendChild(header);
+            header.innerText = data[index]["title"];
+            link.appendChild(header);
+            if (data[index]["polarity"] > 0.4) {  
+                positiveDiv.appendChild(link);
+            } else if (data[index]["polarity"] < -0.4) {
+                negativeDiv.appendChild(link);
+            };
         };
     };
     
     request.send();
 };
+
 
 const searchBtn = document.querySelector("#search-btn");
 
@@ -49,6 +40,5 @@ searchBtn.addEventListener("click", () => {
     
     const input = document.querySelector("#search");
     console.log(input.value);
-    getPositiveRequest(input.value);
-    getNegativeRequest(input.value);
+    parseRedditInfo(input.value);
 });
