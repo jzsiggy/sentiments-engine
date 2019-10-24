35da4ef3c494ec7bb9a72cc6409e90a0ffbde4a4
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-24 10:49:36 -0200

    new sources

--- analyze.py
+++ analyze.py
@@ -1,6 +1,9 @@
 from sentiment import *
 from reddit import *
 from nyt import nyt_article_search
+from crypto_panic import get_cryptopanic_latest
+from crypto_control import get_crypto_control_top_news
+from guardian import guardian_search
 from textblob import TextBlob
 import pandas as pd
 
@@ -19,3 +22,24 @@ def analyze_newYorkTimes_headlines(keyword):
             sentiment = get_avg_sentiment(articles[index]["title"])
             articles[index]["polarity"] = sentiment
     return articles
+
+def analyze_crypto_panic(keyword):
+    submissions = get_cryptopanic_latest(keyword)
+    for index in submissions:
+        sentiment = get_avg_sentiment(submissions[index]["title"])
+        submissions[index]["polarity"] = sentiment
+    return submissions
+
+def analyze_crypto_control():
+    entries = get_crypto_control_top_news()
+    for index in entries:
+        sentiment = get_avg_sentiment(entries[index]["title"])
+        entries[index]["polarity"] = sentiment
+    return entries
+
+def analyze_guardian(keyword):
+    entries = guardian_search(keyword)
+    for index in entries:
+        sentiment = get_avg_sentiment(entries[index]["title"])
+        entries[index]["polarity"] = sentiment
+    return entries
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from analyze import analyze_reddit_headlines, analyze_newYorkTimes_headlines
+from analyze import analyze_reddit_headlines, analyze_newYorkTimes_headlines, analyze_crypto_panic, analyze_crypto_control, analyze_guardian
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
@@ -19,3 +19,17 @@ def reddit():
 def nyt():
     keyword = request.args['keyword']
     return jsonify(analyze_newYorkTimes_headlines(keyword))
+
+@app.route('/crypto-panic')
+def crypto_panic():
+    keyword = request.args['keyword']
+    return jsonify(analyze_crypto_panic(keyword))
+
+@app.route('/crypto-control')
+def crypto_control():
+    return jsonify(analyze_crypto_control())
+
+@app.route('/guardian')
+def guardian():
+    keyword = request.args['keyword']
+    return jsonify(analyze_guardian(keyword))
