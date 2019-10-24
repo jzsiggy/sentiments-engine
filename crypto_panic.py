ae33a5cefcbfc8444b890049b76f7fd82a7022c9
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-24 10:51:02 -0200

    Fixing get_latest_news function

--- crypto_panic.py
+++ crypto_panic.py
@@ -1,14 +1,20 @@
 import json
 import requests
 
-def get__cryptopanic_latest(ticker):
+def get_cryptopanic_latest(ticker):
     url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f21d6681cc7f5638d2ac3006377811ff433a4833&currencies={}&public=true'.format(ticker)
 
     response = requests.get(url)
     response = json.loads(response.text)
     results = response["results"]
 
-    for data in results:
-        print(data["title"])
+    index = 0
+    dic = {}
 
-get_latest("XMR")
+    for post in results:
+        dic[index] = post
+        index+=1
+
+    return dic
+
+get_cryptopanic_latest("BTC")
